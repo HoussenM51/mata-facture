f7f283f1217afc7c9735bb53c6411bdba0c92cc9
HoussenM51 <mhoussen52@gmail.com>
2026-01-29 21:50:17 +0100

    Configure Vite base path for GitHub deployment
    Set the base path for Vite to the GitHub repository.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // AJOUT : Indique à Vite le chemin du dépôt GitHub
+      base: '/mada-facture/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
