51dc8c7e16aa06f64c70f9d7cdd929a8047b3b85
HoussenM51 <mhoussen52@gmail.com>
2026-01-30 08:52:47 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,7 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- AJOUTE CETTE LIGNE ICI
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
