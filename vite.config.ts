9ba5f376d8bf11469195b7e04a82dc6bd416f82a
HoussenM51 <mhoussen52@gmail.com>
2026-01-30 08:39:15 +0100

    Change base path in vite.config.ts
    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial pour GitHub Pages (sous-répertoires)
+  // Remplacez './' par le nom exact de votre dépôt
+  base: '/mata-facture/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
